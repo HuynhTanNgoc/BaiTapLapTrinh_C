fe735972e14f6da0b212859f6eeb44d831e3b21d
HuynhTanNgoc <117697365+HuynhTanNgoc@users.noreply.github.com>
2022-12-13 10:02:24 +0700

    Add files via upload

---
+++ 2.for,while,dowhile.cpp
@@ -0,0 +1,17 @@
+//Xuat ra man hinh so nguyen duong n nho nhat sao cho 1 + 2 +...+ n > 10000.
+#include"stdio.h"
+int main(){
+    //Khai bao bien:
+	int n, S;
+	n = 0;
+	S = 0;
+	//Xu ly va in ra ket qua:
+	while(S<10000){
+		n++;
+		S = S + n;
+	}
+	    printf("Gia tri tong:%d", S);
+	
+	if(S>10000)
+	    printf("\nGia tri n nho nhat la n = %d", n);
+}    
---
+++ 3.for,while,dowhile.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    char kytu;
+    
+    for(kytu=65; kytu<=90; kytu++)
+    //for(kytu='A'; kytu<='Z'; kytu++)
+    printf("%c\n", kytu);
+}
