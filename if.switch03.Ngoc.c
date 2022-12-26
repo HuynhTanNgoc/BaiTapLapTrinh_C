6be0cbe3226ad6cda7f395835c69a3ed1cfd5110
HuynhTanNgoc <117697365+HuynhTanNgoc@users.noreply.github.com>
2022-12-26 10:29:21 +0700

    Update if.switch03.Ngoc.c

--- if.switch03.Ngoc.c
+++ if.switch03.Ngoc.c
@@ -1,26 +1,40 @@
-//Viet chuong trinh giai va bien luan phuong trinh bat nhat ax+b=0
+// Viết chương trình giải và biện luận phương trình bậc nhất ax + b = 0
 #include<stdio.h>
+
 int main() {
-	
-	
+    
+//khai bao bien
     float a, b, x;
     printf("Nhap vao a, b= ");
     scanf("%f%f", &a, &b);
     
-	// Xu ly va in ra ket qua.
-//Gom 2 truong hop a khac 0 va a bang 0	
+// Xu ly va in ra ket qua.
 
-	if(a!=0) x = -b/a; //Neu a khac khong thi ptr co 1 nghiem
-		printf("\nPtrinh co nghiem x = %f", x);	 
-	
-	if(a==0) //Neu a bang 0 thi ptr co 2 truong hop:
-	
-	//TH1 neu b bang 0.
-		if(b==0)
-		printf("\nPtrinh vo so nghiem");
-		
-	//TH2 neu b khac 0.	
-		else if(b!=0)
-		printf("\nPtrinh vo nghiem");		
-	
+  //Gom 2 truong hop a khac 0 va a bang 0	
+     
+    //Neu a khac 0 co 2 truong hop
+    
+     //TH1: b bang 0
+    if(a!=0){
+        if(b==0){
+            printf("\n Ptrinh co nghiem x = 0");
+        }
+     //TH2: b khac 0 
+	    else{
+	        x = -b/a; 
+	    	printf("\nPtrinh co nghiem x = %f", x);	 
+	    }
+    } 
+	//Neu a bang 0 co 2 truong hop:
+	if(a==0) {
+    	//TH1 neu b bang 0.
+	    if(b==0){
+	    	printf("\nPtrinh vo so nghiem");
+	    }
+    	//TH2 neu b khac 0.	
+		else
+	    	printf("\nPtrinh vo nghiem");		
+	    
+	    
+	}
 }
